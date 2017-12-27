8a298a980a7e7e1c1fb71acc8c13808ac41acad6
mercifulking <mercifulking@163.com>
2017-12-27 19:57:24 +0800

    challenge1

--- calculator.py
+++ calculator.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
-
 import sys
 
-def income_tax_payable(income):
-    return income - 3500
 
-def tax_payable(income_tax_payable, tax_rate, quick_deduction):
-    return income_tax_payable * tax_rate - quick_deduction
-
-if __name__ == '__main__':
-    
-    if len(sys.argv)>2 or len(sys.argv) == 1:
-        raise ValueError('print("Parameter Error")')
-    IncomeTax = income_tax_payalbe(sys.argv[1]) 
-    if 0< IncomeTax <= 1500:
-        tax_rate = 0.03 
-        quick_deduction = 0
-    elif IncomeTax <= 4500:
-        tax_rate = 0.10
-        quick_deduction = 105
-    elif IncomeTax <= 9000:
-        tax_rate = 0.20
-        quick_deduction = 555
-    elif IncomeTax <= 35000:
-        tax_rate = 0.30
-        quick_deduction = 2755
-    elif IncomeTax <= 55000:
-        tax_rate = 0.30
-        quick_deduction = 2755
-    elif IncomeTax <= 80000:
-        tax_rate = 0.35
-        quick_deduction = 5505
+def main():
+    if len(sys.argv) != 2:
+        print('Parameter Error')
+        exit()
+    try:
+        income = int(sys.argv[1])
+    except ValueError:
+        print('Parameter Error')
+        exit()
+    value = income - 3500
+    if value <= 0:
+        result = income 
+    elif value <= 1500:
+        result = value * 0.03 - 0
+    elif value <= 4500:
+        result = value * 0.10 - 105
+    elif value <= 9000:
+        result = value * 0.20 - 555
+    elif value <= 35000:
+        result = value * 0.25 - 1005
+    elif value <= 55000:
+        result = value * 0.30 - 2755
+    elif value <= 80000:
+        result = value * 0.35 - 5505
     else:
-        tax_rate = 0.45
-        quick_deduction = 13505
+        result = value * 0.45 - 13505
+    print('{:.2f}'.format(result))
+  
+if __name__ == '__main__':
+    main()
 
-    print(tax_payable(IncomTax, tax_rate, quick_deduction))
